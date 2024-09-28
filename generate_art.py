106e28d4253e85cec071665885870aefc7c82318
ysasano <32053111+ysasano@users.noreply.github.com>
2024-09-28 12:14:44 +0900

    横幅もうちょい行けるんじゃない？

--- generate_art.py
+++ generate_art.py
@@ -10,7 +10,7 @@ def generate_ascii_art():
     
     # 各行の幅を50文字以内に制限
     limited_art += "```\n"
-    limited_art = "\n".join([line[:50] for line in art.splitlines()])
+    limited_art = "\n".join([line[:100] for line in art.splitlines()])
     limited_art += "\n```"
     return limited_art
 
